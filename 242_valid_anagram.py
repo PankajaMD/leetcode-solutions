ae0ae24a6c8721eb050e9cfa67f71c20f99b8223
Pankaja <pankajamd714@gmail.com>
2026-09-11 21:49:58 +0530

    Add Valid Anagram solution

---
+++ 242_valid_anagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        nums = [0]*26
+        for i in range(len(s)):
+            nums[ord(s[i]) - ord('a')] += 1
+            nums[ord(t[i]) - ord('a')] -= 1
+
+        for count in nums:
+            if count != 0:
+                return False
+        
+        return True
