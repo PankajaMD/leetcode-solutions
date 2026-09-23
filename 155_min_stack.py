97e805478554bf8fde6a9955a665ca35c290cab0
Pankaja <pankajamd714@gmail.com>
2026-09-23 19:07:28 +0530

    Add Min Stack solution

---
+++ 155_min_stack.py
@@ -0,0 +1,33 @@
+class MinStack:
+
+    def __init__(self):
+       self.head = None 
+
+    def push(self, val: int) -> None:
+        if self.head is None:
+            self.head = Node(val, val, None)
+        else:
+            self.head = Node(val, min(val, self.head.min), self.head)
+
+    def pop(self) -> None:
+        self.head = self.head.next
+
+    def top(self) -> int:
+        return self.head.val
+
+    def getMin(self) -> int:
+        return self.head.min
+
+class Node:
+    def __init__(self, val, min, next):
+        self.val = val
+        self.min = min
+        self.next = next
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
